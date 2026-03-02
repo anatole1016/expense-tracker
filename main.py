a7ac1e841f9983bbc5c294e798b82696323ce639
Anatolii Kucheruk <anatoliy.kucheruck@gmail.com>
2026-03-02 23:02:48 +0000

    Add storage.py for loading/saving expenses
    Implement load_expenses() and save_expenses() to read/write expenses.json with error handling.

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
+from storage import load_expenses, save_expenses
+from models import Expense
+
 def main():
     print("Welcome to Anatolii's Expense Tracker!")
 
+expenses = load_expenses()
+
+def add_expenses():
+    try:
+        amount  = float(input("Enter expense amount please: "))
+        category = input("Enter expense category please: ")
+        description = input("Enter expense description please")
+        date = input("Enter expense date (YYYY-MM-DD): ")
+    
+        expense = {
+            "amount": amount,
+            "category": category,
+            "description": description,
+            "date": date
+        }
+        expenses.append(expense)
+        save_expenses(expenses)
+        print("Expense added successfully!\n")
+    except ValueError:
+        print("Invalid amount. Try again.\n")
+        
+
+def list_expenses():
+    if not expenses:
+        print("No expenses found.\n")
+        return
+    for i, e in enumerate(expenses, 1):
+        print(f"{i}. {e['date']} | {e['category']} | ${e['amount']} | {e['description']}")
+    print()
+
+def main_menu():
+    while True:
+        print("Expense Tracker  Main Menu")
+        print("1. Add Expense")
+        print("2. List Expenses")
+        print("3. Quit")
+        choice = input("Select an option: ")
+        
+        if choice == "1":
+            add_expenses()
+        elif choice == "2":
+            list_expenses()
+        elif choice == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.\n")
+    
 if __name__ == "__main__":
     main()
--- storage.py
+++ storage.py
@@ -1,5 +1,20 @@
+import json
+import os
+
+EXPENSES_FILE = 'expenses.json'
+
 def load_expenses():
+    if os.path.exists(EXPENSES_FILE):
+        try:
+            with open(EXPENSES_FILE, "r") as file:
+                return json.load(file)
+        except (json.JSONDecodeError, IOError):
+            print("Error when loading expenses files")
     return []
 
 def save_expenses(expenses):
-    pass
+    try:
+        with open(EXPENSES_FILE, 'w') as file:
+            json.dump(expenses, file, indent=4)
+    except IOError:
+        print("Error when saving expenses file")
